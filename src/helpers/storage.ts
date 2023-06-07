ce5a668f1c141295f4fd7f1d28bd1b53ec911689
Henrik Hüttemann <henrik.huettemann@verdigado.com>
2023-06-07 18:07:36 +0200

    Add Storage helper

---
+++ src/helpers/storage.ts
@@ -0,0 +1,26 @@
+import { DataSource } from 'typeorm'
+import { IdMapping } from '../entity/IdMapping'
+import { Membership } from '../entity/Membership'
+
+const AppDataSource = new DataSource({
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: [IdMapping, Membership],
+  synchronize: true,
+  logging: false,
+})
+
+export function getMapping(id: string, type: number) {
+  return AppDataSource.manager.findOneBy(IdMapping, {
+    rcId: id,
+    type: type,
+  })
+}
+
+export async function save(entity: IdMapping | Membership) {
+  await AppDataSource.manager.save(entity)
+}
+
+export async function getAccessToken(id: string) {
+  return (await getMapping(id, 0))?.accessToken
+}
