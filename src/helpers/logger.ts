1aff3561ae6a81499b966de24e1090fc8b9526e2
Henrik Hüttemann <henrik.huettemann@verdigado.com>
2023-09-13 16:52:31 +0200

    Fix test logs being written to file

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -4,8 +4,15 @@ export default winston.createLogger({
   level: 'debug',
   transports: [
     new winston.transports.Console(),
-    new winston.transports.File({ filename: 'warn.log', level: 'warn' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({
+      filename: 'warn.log',
+      level: 'warn',
+      silent: process.env.NODE_ENV === 'test',
+    }),
+    new winston.transports.File({
+      filename: 'combined.log',
+      silent: process.env.NODE_ENV === 'test',
+    }),
   ],
   format: winston.format.combine(
     winston.format.colorize({ all: true }),
