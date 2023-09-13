29036deb58090831bd5babd5dabb69f7dc261bc0
Henrik Hüttemann <henrik.huettemann@verdigado.com>
2023-09-13 16:41:41 +0200

    Write logs to files

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -2,7 +2,11 @@ import winston from 'winston'
 
 export default winston.createLogger({
   level: 'debug',
-  transports: [new winston.transports.Console()],
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'warn.log', level: 'warn' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
   format: winston.format.combine(
     winston.format.colorize({ all: true }),
     winston.format.simple()
