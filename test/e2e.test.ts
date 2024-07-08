6bb08fdf9e01e045e4d4bf5c4d25be31904943ec
Henrik Hüttemann <henrik.huettemann@verdigado.com>
2024-07-08 17:16:21 +0200

    Add message test

--- test/e2e.test.ts
+++ test/e2e.test.ts
@@ -1,9 +1,12 @@
 import { beforeAll, describe, expect, test } from '@jest/globals'
-import { axios } from '../src/helpers/synapse'
-import { entities } from '../src/Entities'
 import lineByLine from 'n-readlines'
+import { entities } from '../src/Entities'
+import { IdMapping } from '../src/entity/IdMapping'
+import { MatrixMessage, RcMessage } from '../src/handlers/messages'
 import { MatrixRoom, RcRoom } from '../src/handlers/rooms'
 import { MatrixUser, RcUser } from '../src/handlers/users'
+import { getMapping, getRoomId, initStorage } from '../src/helpers/storage'
+import { axios } from '../src/helpers/synapse'
 
 describe('rooms', () => {
   const rcRooms: RcRoom[] = []
@@ -46,5 +49,44 @@ describe('users', () => {
   test('equal length', async () => {
     const IGNORED_USERS = 1
     expect(matrixUsers.length).toBe(rcUsers.length - IGNORED_USERS)
+  })
+})
+
+type Message = {
+  rc: RcMessage
+  matrix?: MatrixMessage
+  mapping?: IdMapping
+}
+
+describe('messages', () => {
+  const messages: Message[] = []
+
+  beforeAll(async () => {
+    await initStorage()
+    const rl = new lineByLine(`./inputs/${entities.messages.filename}`)
+    let line: false | Buffer
+    while ((line = rl.next())) {
+      const message: Message = { rc: JSON.parse(line.toString()) }
+      message.mapping = (await getMapping(message.rc._id, 2)) || undefined
+      if (message.mapping) {
+        const roomId = await getRoomId(message.rc.rid)
+        const messageId = message.mapping.matrixId
+
+        message.matrix =
+          (
+            await axios.get(
+              `/_matrix/client/v3/rooms/${roomId}/event/${messageId}`
+            )
+          ).data || undefined
+      }
+      messages.push(message)
+    }
+  })
+
+  test('equal length', async () => {
+    const IGNORED_MESSAGES = 5
+    expect(messages.filter((message) => !!message.mapping).length).toBe(
+      messages.length - IGNORED_MESSAGES
+    )
   })
 })
